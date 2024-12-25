528c7a223abf978d4df13fb8d56380d6c1447d57
Ilyas chaoui <144786151+ichaoui56@users.noreply.github.com>
2024-12-25 21:53:27 +0100

    Configure routing and integrate feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'game', pathMatch: 'full' },
+  { path: 'game', loadChildren: () => import('./features/game/game.module').then(m => m.GameModule) },
+  { path: 'score', loadChildren: () => import('./features/score/score.module').then(m => m.ScoreModule) },
+  { path: 'result', loadChildren: () => import('./features/result/result.module').then(m => m.ResultModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
